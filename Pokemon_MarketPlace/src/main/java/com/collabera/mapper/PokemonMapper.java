7b559e27a9e47238ab78c7ffb6ac99ec1df20049
ValorWind1 <skyjdani@gmail.com>
2019-10-15 14:46:59 -0400

    Mapper draft 01

--- Pokemon_MarketPlace/src/main/java/com/collabera/mapper/PokemonMapper.java
+++ Pokemon_MarketPlace/src/main/java/com/collabera/mapper/PokemonMapper.java
@@ -1,5 +1,32 @@
 package com.collabera.mapper;
 
-public class PokemonMapper {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Component;
+
+import com.collabera.dto.ItemDTO;
+import com.collabera.dto.PokemonDTO;
+import com.collabera.dto.ShiniesDTO;
+
+@Component
+public class PokemonMapper {
+	
+	public Pokemon toEntity(PokemonDTO dto) {
+		Pokemon pokemon = new Pokemon();
+		
+		pokemon.setName(dto.ItemDTO());
+		pokemon.setCategory(dto.PokemonDTO());
+		pokemon.setName(dto.ShiniesDTO());
+		return pokemon ; 
+
+}
+
+	public PokemonDTO toDto(Pokemon entity) {
+		PokemonDTO pokemon = new PokemonDTO();
+		pokemon.setMovieId(entity.getId());
+		pokemon.setName(entity.getName());
+		pokemon.setCategory(entity.getCategory());	
+		return pokemon;
+	}
 }
