e36dcace52f2ec2d9180b464d81daaa9ec767cde
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2019-10-15 16:42:07 -0400

    Update PokeService.java

--- Pokemon_MarketPlace/src/main/java/com/collabera/services/PokeService.java
+++ Pokemon_MarketPlace/src/main/java/com/collabera/services/PokeService.java
@@ -1,27 +1,63 @@
 package com.collabera.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.collabera.mha.dto.StudentDTO;
+import com.collabera.mha.model.StudentModel;
 import com.collabera.mongodbtest.model.Jojo;
 
 public class PokeService {
 
 	@Autowired
-	private final PokeRepo repo;
-
-	public PokeService(PokeRepo repo) {
+	private final PokeRepo Repo;
+	private final PokemonMapper Map;
+	
+	public PokeService(PokeRepo repo, PokemonMapper map) {
 		super();
-		this.repo = repo;
+		this.Repo = repo;
+		Map = map;
 	}
-	
-	List<PokemonDTO> getAll() {
+
+	public List<PokemonDTO> getAll() {
 		
-		return repo.findAll();
+		return Repo.findAll();
 	}
 	
-	
+	public PokemonDTO findById(int id) {
+		Optional<Pokemon> pokeOP = Repo.findById(id);
+		if (pokeOP.isPresent()) {
+			return Map.toDTO(pokeOP.get());
+		}
+		return null;
+	}
+
+	public PokemonDTO save(PokemonDTO poke) {
+		Pokemon entity = Map.toEntity(poke);
+		Pokemon saved = Repo.insert(entity);
+		return Map.toDTO(saved);
+	}
+
+	public PokemonDTO update(PokemonDTO poke) {
+		int id = poke.getId();
+		Optional<Pokemon> findById = Repo.findById(id);
+		if (findById.isPresent()) {
+			Pokemon uHero = findById.get();
+			uHero.setName(poke.getName());
+			uHero.setHeroName(poke.getHeroName());
+			uHero.setQuirk(poke.getQuirk());
+			Pokemon saved = Repo.save(uHero);
+			return Map.toDTO(saved);
+		} else { 
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public void deleteById(int id) {
+		Repo.deleteById(id);
+	}
 	
 	
 	
