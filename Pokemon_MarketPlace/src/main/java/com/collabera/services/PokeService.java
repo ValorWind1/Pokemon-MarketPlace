332c3d82ba62f089b94b946cf444dd40b7195ebc
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2019-10-15 16:21:45 -0400

    Update PokeService.java

--- Pokemon_MarketPlace/src/main/java/com/collabera/services/PokeService.java
+++ Pokemon_MarketPlace/src/main/java/com/collabera/services/PokeService.java
@@ -1,10 +1,28 @@
 package com.collabera.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.collabera.mongodbtest.model.Jojo;
 
 public class PokeService {
 
 	@Autowired
-	PokeRepo repo;
+	private final PokeRepo repo;
+
+	public PokeService(PokeRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	List<PokemonDTO> getAll() {
+		
+		return repo.findAll();
+	}
+	
+	
+	
+	
 	
 }
