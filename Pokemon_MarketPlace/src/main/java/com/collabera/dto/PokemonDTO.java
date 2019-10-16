3cbe47ef235dadaeaaff5dab264bd20fc9b0462e
stanmill <stanley.pena29@gmail.com>
2019-10-16 10:01:58 -0400

    updated the DTO

--- Pokemon_MarketPlace/src/main/java/com/collabera/dto/PokemonDTO.java
+++ Pokemon_MarketPlace/src/main/java/com/collabera/dto/PokemonDTO.java
@@ -1,98 +1,118 @@
 package com.collabera.dto;
 
+import java.math.BigInteger;
+
+import com.collabera.model.Sprite;
+
 public class PokemonDTO {
 	
-	private Integer id; 
-	private String national_number; 
-	private Sprites sprites; 
+	private BigInteger id;
+	private String nationalNum;
 	private String name;
-	private String type;
-	
-	private Integer total; 
-	private Integer hp ; 
-	private Integer attack; 
+	private String[] type;
+	private Sprite sprite;
+	private Integer total;
+	private Integer hp;
+	private Integer attack;
 	private Integer defense;
-	private Integer sp_atk;
-	private Integer sp_def;
 	private Integer speed;
-	public Integer getId() {
+	
+	public PokemonDTO() {
+		
+	}
+	
+	public PokemonDTO(BigInteger id, String nationalNum, String name, String[] type, Sprite sprite, 
+			Integer total, Integer hp, Integer attack, Integer defense, Integer speed) {
+		super();
+		this.id = id;
+		this.nationalNum = nationalNum;
+		this.name = name;
+		this.type = type;
+		this.sprite = sprite;
+		this.total = total;
+		this.hp = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+	}
+
+	public BigInteger getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+
+	public void setId(BigInteger id) {
 		this.id = id;
 	}
-	public String getNational_number() {
-		return national_number;
+
+	public String getNationalNum() {
+		return nationalNum;
 	}
-	public void setNational_number(String national_number) {
-		this.national_number = national_number;
+
+	public void setNationalNum(String nationalNum) {
+		this.nationalNum = nationalNum;
 	}
-	public Sprites getSprites() {
-		return sprites;
-	}
-	public void setSprites(Sprites sprites) {
-		this.sprites = sprites;
-	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getType() {
+
+	public String[] getType() {
 		return type;
 	}
-	public void setType(String type) {
+
+	public void setType(String[] type) {
 		this.type = type;
 	}
+
+	public Sprite getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
 	public Integer getTotal() {
 		return total;
 	}
+
 	public void setTotal(Integer total) {
 		this.total = total;
 	}
+
 	public Integer getHp() {
 		return hp;
 	}
+
 	public void setHp(Integer hp) {
 		this.hp = hp;
 	}
+
 	public Integer getAttack() {
 		return attack;
 	}
+
 	public void setAttack(Integer attack) {
 		this.attack = attack;
 	}
+
 	public Integer getDefense() {
 		return defense;
 	}
+
 	public void setDefense(Integer defense) {
 		this.defense = defense;
 	}
-	public Integer getSp_atk() {
-		return sp_atk;
-	}
-	public void setSp_atk(Integer sp_atk) {
-		this.sp_atk = sp_atk;
-	}
-	public Integer getSp_def() {
-		return sp_def;
-	}
-	public void setSp_def(Integer sp_def) {
-		this.sp_def = sp_def;
-	}
+
 	public Integer getSpeed() {
 		return speed;
 	}
+
 	public void setSpeed(Integer speed) {
 		this.speed = speed;
 	}
-	
-	
-	
-	
-	
-	
-	
-
 }
--- Pokemon_MarketPlace/src/main/java/com/collabera/dto/ShiniesDTO.java
+++ Pokemon_MarketPlace/src/main/java/com/collabera/dto/ShiniesDTO.java
@@ -1,5 +1,119 @@
 package com.collabera.dto;
 
+import java.math.BigInteger;
+
+import com.collabera.model.Sprite;
+
 public class ShiniesDTO {
+	
+	private BigInteger id;
+	private String nationalNum;
+	private String name;
+	private String[] type;
+	private Sprite sprite;
+	private Integer total;
+	private Integer hp;
+	private Integer attack;
+	private Integer defense;
+	private Integer speed;
+	
+	public ShiniesDTO() {
+		
+	}
+	
+	public ShiniesDTO(BigInteger id, String nationalNum, String name, String[] type, 
+			Sprite sprite, Integer total, Integer hp, Integer attack, Integer defense, Integer speed) {
+		super();
+		this.id = id;
+		this.nationalNum = nationalNum;
+		this.name = name;
+		this.type = type;
+		this.sprite = sprite;
+		this.total = total;
+		this.hp = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+	}
+
+	public BigInteger getId() {
+		return id;
+	}
+
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+
+	public String getNationalNum() {
+		return nationalNum;
+	}
+
+	public void setNationalNum(String nationalNum) {
+		this.nationalNum = nationalNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getType() {
+		return type;
+	}
+
+	public void setType(String[] type) {
+		this.type = type;
+	}
+
+	public Sprite getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public Integer getHp() {
+		return hp;
+	}
+
+	public void setHp(Integer hp) {
+		this.hp = hp;
+	}
+
+	public Integer getAttack() {
+		return attack;
+	}
+
+	public void setAttack(Integer attack) {
+		this.attack = attack;
+	}
+
+	public Integer getDefense() {
+		return defense;
+	}
+
+	public void setDefense(Integer defense) {
+		this.defense = defense;
+	}
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}	
 
 }
