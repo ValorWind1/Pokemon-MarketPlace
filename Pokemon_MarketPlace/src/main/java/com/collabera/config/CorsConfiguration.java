4375432700662be8b26a9d83b29ef962aebca67a
ValorWind1 <skyjdani@gmail.com>
2019-10-16 14:52:03 -0400

    search bar item

--- Pokemon_MarketPlace/src/main/java/com/collabera/config/CorsConfiguration.java
+++ Pokemon_MarketPlace/src/main/java/com/collabera/config/CorsConfiguration.java
@@ -1,5 +1,17 @@
 package com.collabera.config;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+  
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true).allowedMethods("GET","PUT","POST","HEAD","DELETE","OPTIONS");
+    }
 }
+
