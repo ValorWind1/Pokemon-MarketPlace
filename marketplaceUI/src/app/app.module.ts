df94167ec2c577ba2a4b26aa9c9e8e18ebb24d55
stanmill <stanley.pena29@gmail.com>
2019-10-16 11:54:26 -0400

    folders done

--- marketplaceUI/src/app/app.module.ts
+++ marketplaceUI/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
