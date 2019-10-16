f6b716eec0b7b37c6a5d6c3009543f4062d5d0f4
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2019-10-16 11:46:13 -0400

    started working on service

--- marketplaceUI/src/app/app.module.ts
+++ marketplaceUI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
