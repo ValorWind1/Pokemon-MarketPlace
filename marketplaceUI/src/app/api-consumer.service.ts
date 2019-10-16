b0b25a6544c00cf37edba11422aba1316e678d40
stanmill <stanley.pena29@gmail.com>
2019-10-16 13:29:58 -0400

    update

--- marketplaceUI/src/app/api-consumer.service.ts
+++ marketplaceUI/src/app/api-consumer.service.ts
@@ -7,62 +7,62 @@ import { Subject, Observable } from 'rxjs';
 })
 export class ApiConsumerService {
 
-  // @Input() Isshiny: boolean;
+  @Input() Isshiny: boolean;
 
-  // private BaseEndpoint = 'http://localhost:8080/api/';
+  private BaseEndpoint = 'http://localhost:8080/api/';
 
-  // httpOptions = {
-  //   headers: new HttpHeaders({
-  //     'Content-Type': 'application/json'
-  //   })
-  // };
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-  // constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  // endpoint() {
-  //   if (this.Isshiny){
-  //     return this.BaseEndpoint + 'shiny';
-  //   }
-  // }
+  endpoint() {
+    if (this.Isshiny){
+      return this.BaseEndpoint + 'shiny';
+    }
+  }
 
 
 
-  // heroUpdatedSubject: Subject<Hero> = new Subject<Hero>();
+  heroUpdatedSubject: Subject<Hero> = new Subject<Hero>();
 
-  // subscribeToHeroUpdates(): Subject<Hero> {
-  //   return this.heroUpdatedSubject;
-  // }
+  subscribeToHeroUpdates(): Subject<Hero> {
+    return this.heroUpdatedSubject;
+  }
 
-  // getHeroes(): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
 
-  //   return (this.http.get<Hero[]>(this.endpoint));
-  // }
+    return (this.http.get<Hero[]>(this.endpoint));
+  }
 
-  // getHero(id: number): Observable<Hero> {
-  //   const url = `${this.endpoint}/${id}`;
-  //   return this.http.get<Hero>(url);
-  // }
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.endpoint}/${id}`;
+    return this.http.get<Hero>(url);
+  }
 
-  // addHero(hero: Hero): Observable<Hero> {
-  //   console.log('inside add method');
-  //   return this.http.post<any>(this.endpoint, JSON.stringify(hero), this.httpOptions);
-  // }
+  addHero(hero: Hero): Observable<Hero> {
+    console.log('inside add method');
+    return this.http.post<any>(this.endpoint, JSON.stringify(hero), this.httpOptions);
+  }
 
-  // updateHero(hero: Hero): Observable<any> {
-  //   return this.http.put(this.endpoint, JSON.stringify(hero), this.httpOptions);
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.endpoint, JSON.stringify(hero), this.httpOptions);
 
-  // }
+  }
 
-  // deleteHero(hero: Hero | number): Observable<Hero> {
-  //   const id = typeof hero === 'number' ? hero : hero.id;
-  //   const url = `${this.endpoint}/${id}`;
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.endpoint}/${id}`;
 
-  //   return this.http.delete<Hero>(url, this.httpOptions);
-  // }
+    return this.http.delete<Hero>(url, this.httpOptions);
+  }
 
-  // heroUpdated(hero: Hero) {
-  //   this.heroUpdatedSubject.next(hero);
-  // }
+  heroUpdated(hero: Hero) {
+    this.heroUpdatedSubject.next(hero);
+  }
 
 }
 
