847cfb12c1cbf06f50998b555a5390f4f19031ed
ValorWind1 <skyjdani@gmail.com>
2019-10-16 11:53:21 -0400

    item component

--- marketplaceUI/src/app/app.module.ts
+++ marketplaceUI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PokeItemsComponent } from './poke-items/poke-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PokeItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ marketplaceUI/src/app/poke-items/poke-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-items',
+  templateUrl: './poke-items.component.html',
+  styleUrls: ['./poke-items.component.css']
+})
+export class PokeItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
